4bdeeeb3d7f741ec8583e3834b652a678db48fd0
LI SHENG <li.sheng@carsales.com.au>
2020-12-23 23:17:18 +1100

    refactored dataloader a little bit

--- ZendeskSearch/DataLoader.cs
+++ ZendeskSearch/DataLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using ZendeskSearch.Model;
@@ -13,25 +14,37 @@ namespace ZendeskSearch
     public class DataLoader : IDataLoader
     {
         private const string FilePath = "Datastore";
-        private const string UsersFile = "users.json";
-        private const string OrganizationsFile = "organizations.json";
-        private const string TicketsFile = "tickets.json";
+
+        private Dictionary<string, string> DataFiles = new Dictionary<string, string>
+        {
+            {  nameof(User), "users.json" },
+            {  nameof(Ticket), "tickets.json" },
+            {  nameof(Organization), "organizations.json" }
+        };
         
         public Zendesk Load()
         {
-            var usersJsonString = File.ReadAllText($"{FilePath}/{UsersFile}");
-            var organizationsJsonString = File.ReadAllText($"{FilePath}/{OrganizationsFile}");
-            var ticketsJsonString = File.ReadAllText($"{FilePath}/{TicketsFile}");
-
-            var users = JsonConvert.DeserializeObject<List<User>>(usersJsonString);
-            var organizations = JsonConvert.DeserializeObject<List<Organization>>(organizationsJsonString);
-            var tickets = JsonConvert.DeserializeObject<List<Ticket>>(ticketsJsonString);
-
             var dataset = new Zendesk();
-            dataset.Data.Add(nameof(User), users);
-            dataset.Data.Add(nameof(Organization), organizations);
-            dataset.Data.Add(nameof(Ticket), tickets);
+            dataset.Data.Add(nameof(User), Load<User>());
+            dataset.Data.Add(nameof(Ticket), Load<Ticket>());
+            dataset.Data.Add(nameof(Organization), Load<Organization>());
             return dataset;
+        }
+
+        private List<T> Load<T>()
+        {
+            try
+            {
+                var file = DataFiles[typeof(T).Name];
+                var usersJsonString = File.ReadAllText($"{FilePath}/{file}");
+                var items = JsonConvert.DeserializeObject<List<T>>(usersJsonString);
+
+                return items;
+            }
+            catch(Exception e)
+            {
+                return new List<T>();
+            }
         }
     }
 }
